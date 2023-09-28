3b40893f0d17511f00e6a01b78d8bb5ef54841bd
Aepex25 <angelestevezsalinas@gmail.com>
2023-09-28 00:10:25 -0400

    icon animation and buttons lock at 5 points

--- app.js
+++ app.js
@@ -7,14 +7,17 @@
 const btnRock = document.getElementById("rock");
 const btnPaper = document.getElementById("paper");
 const btnScissors = document.getElementById("scissors");
+const rockPc = document.querySelector(".pc-choice-rock");
+const paperPc = document.querySelector(".pc-choice-paper");
+const scissorsPc = document.querySelector(".pc-choice-scissors");
 const div_playerScore = document.querySelector(".player-score");
 const div_computerScore = document.querySelector(".computer-score");
 let spanPlayerScore = document.querySelector(".p-score");
 let spanComputerScore = document.querySelector(".c-score");
 let displayResult = document.querySelector(".results");
-const playerIconSelection = document.querySelector(".player-choices-container");
 const playAgainBtn = document.querySelector(".play-again-btn");
-console.log(playAgainBtn);
+const playerIconContainer = document.querySelector(".player-choices-container");
+const pcIconsContainer = document.querySelector(".computer-choices-container");
 
 ///// Scores /////
 
@@ -28,6 +31,30 @@ const choices = ["rock", "paper", "scissors"];
 ///// Computer Choice /////
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+///// Computer Selection Color //////
+
+function pcIconColors(selection) {
+  if (selection == "rock") {
+    rockPc.classList.add("computer-choice");
+    setTimeout(function () {
+      rockPc.classList.add("no-computer-choice");
+    }, 800);
+  } else if (selection == "paper") {
+    paperPc.classList.add("computer-choice");
+    setTimeout(function () {
+      paperPc.classList.add("no-computer-choice");
+    }, 800);
+  } else if (selection == "scissors") {
+    scissorsPc.classList.add("computer-choice");
+    setTimeout(function () {
+      scissorsPc.classList.add("no-computer-choice");
+    }, 800);
+  }
+  setTimeout(function () {
+    clearPcIcons();
+  }, 1000);
 }
 
 ///// Player Choice /////
@@ -77,6 +104,7 @@ function lose() {
 
 function playRound(playerSelection, computerSelection) {
   computerSelection = getComputerChoice();
+  pcIconColors(computerSelection);
   let result = checkWinner(playerSelection, computerSelection);
   if (result === "tie") {
     displayResult.textContent = "IT'S A TIE!!!";
@@ -94,10 +122,27 @@ function playRound(playerSelection, computerSelection) {
 function game(playerSelection) {
   playRound(playerSelection);
   if (playerScore === 5) {
+    disableButtons();
     displayResult.textContent = `YOU WIN THE GAME!!!! Final Score - Player ( ${playerScore} ) : Computer ( ${computerScore} )`;
   } else if (computerScore === 5) {
+    disableButtons();
     displayResult.textContent = `YOU LOSE THE GAME!! Final Score - Computer ( ${computerScore} ) : Player ( ${playerScore} )`;
   }
+}
+
+///// Clear Pc Icons /////
+
+function clearPcIcons() {
+  rockPc.classList.remove("computer-choice", "no-computer-choice");
+  paperPc.classList.remove("computer-choice", "no-computer-choice");
+  scissorsPc.classList.remove("computer-choice", "no-computer-choice");
+}
+
+///// Disable Buttons /////
+
+function disableButtons() {
+  playerIconContainer.classList.add("disable-buttons");
+  pcIconsContainer.classList.add("disable-buttons");
 }
 
 ///// Play again btn /////
@@ -108,6 +153,8 @@ const playAgain = function () {
   spanPlayerScore.textContent = playerScore;
   spanComputerScore.textContent = computerScore;
   displayResult.textContent = "Results";
+  playerIconContainer.classList.remove("disable-buttons");
+  pcIconsContainer.classList.remove("disable-buttons");
 };
 
 playAgainBtn.addEventListener("click", playAgain);
